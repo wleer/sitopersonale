118ece0128cfcb9e0a9d0e8ea850e6767fe61f3e
Cosimo Spina <csxleer@gmail.com>
2026-02-04 11:25:42 +0100

    Ho aggiunto la sezione hero

--- src/main.js
+++ src/main.js
@@ -9,7 +9,7 @@ import "bootstrap-icons/font/bootstrap-icons.css";
 
 // import 'bootstrap/js/dist/alert';
 import "bootstrap/js/dist/button";
-// import 'bootstrap/js/dist/carousel';
+import 'bootstrap/js/dist/carousel';
 import "bootstrap/js/dist/collapse";
 // import 'bootstrap/js/dist/dropdown';
 // import 'bootstrap/js/dist/modal';
